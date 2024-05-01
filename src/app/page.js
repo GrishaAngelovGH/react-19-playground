bc9030fc0f1d620121155ca92c03dae030796c03
Grisha Angelov <grisha.angelov89@gmail.com>
2024-05-01 10:28:17 +0300

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,21 @@
+import styles from "./page.module.css"
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+      </div>
+    </main>
+  )
+}
